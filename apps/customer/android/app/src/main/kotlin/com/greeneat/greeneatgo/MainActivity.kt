febcef816757f744e862133b83530d600bb31024
yonguk <lovely20uk@gmail.com>
2026-07-12 10:37:20 +0900

    push

---
+++ apps/customer/android/app/src/main/kotlin/com/greeneat/greeneatgo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.greeneat.greeneatgo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- apps/customer/android/app/src/main/kotlin/com/greeneatgo/greeneatgo_customer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.greeneatgo.greeneatgo_customer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
